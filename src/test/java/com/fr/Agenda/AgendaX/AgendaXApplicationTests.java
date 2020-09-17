bb5a62f15078ddc3f78601a2f5a6dccba9763112
AmbroiseRENE <ambroise.rene.73@gmail.com>
2020-09-17 09:15:12 +0200

    parametrage who permite the maven install

--- src/test/java/com/fr/Agenda/AgendaX/AgendaXApplicationTests.java
+++ src/test/java/com/fr/Agenda/AgendaX/AgendaXApplicationTests.java
@@ -1,38 +1,38 @@
 package com.fr.Agenda.AgendaX;
 
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//
-//@SpringBootTest(classes = AgendaXApplication.class)
-//@AutoConfigureMockMvc
-//public class AgendaXApplicationTests {
-//
-//	@Autowired
-//	protected MockMvc mvc;
-//	
-//	protected ObjectMapper mapper = new ObjectMapper();
-//	
-//	@Test
-//	public void exampleOfTestCase() {
-//		assertTrue(true);
-//	}
-//}
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class AgendaXApplicationTests {
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+@SpringBootTest(classes = AgendaXApplication.class)
+@AutoConfigureMockMvc
+public class AgendaXApplicationTests {
+
+	@Autowired
+	protected MockMvc mvc;
+	
+	protected ObjectMapper mapper = new ObjectMapper();
+	
 	@Test
-	void contextLoads() {
+	public void exampleOfTestCase() {
+		assertTrue(true);
 	}
-
 }
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class AgendaXApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
